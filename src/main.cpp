e3d402603eef9dffc5e181423084ebb10350964d
umarmghl <umarmghl001@gmail.com>
2026-01-04 20:44:07 +0500

    - display students

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+const int MAX = 100;
+int rollNo[MAX], meals[MAX];
+string name[MAX];
+int totalStudents = 0;
+
 int main() {
     int choice;
     
@@ -11,12 +16,32 @@ int main() {
         cout << "1. Add Student\n";
         cout << "2. Display All Students\n";
         cout << "3. Update Student Meals\n";
-        
+        cout << "6. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice) {
+            
+            case 2: displayStudents(); break;
+            
+            default: cout << "Invalid choice! Try again.\n";
+        }
 
         
     } while (choice != 6);
 
     return 0;
+}
+
+
+void displayStudents() {
+    if (totalStudents == 0) {
+        cout << "No records found!\n";
+        return;
+    }
+
+    cout << "\nRollNo\tName\tMeals\n";
+    for (int i = 0; i < totalStudents; i++) {
+        cout << rollNo[i] << "\t" << name[i] << "\t" << meals[i] << endl;
+    }
 }
