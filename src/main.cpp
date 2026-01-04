e5c12d07f6b8be43932872e770ca390d13613f51
umarmghl <umarmghl001@gmail.com>
2026-01-04 21:24:20 +0500

    - student add, delete, update function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int totalStudents = 0;
 
 void loadStudents();
 void saveStudents();
+void displayStudents();
 
 void addStudent() {
     if (totalStudents >= MAX) {
@@ -45,6 +46,34 @@ void updateMeals() {
         cout << "Student not found!\n";
 }
 
+
+void deleteStudent() {
+    int r, index = -1;
+    cout << "Enter Roll No to delete record: ";
+    cin >> r;
+
+    for (int i = 0; i < totalStudents; i++) {
+        if (rollNo[i] == r) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1) {
+        cout << "Student not found!\n";
+        return;
+    }
+
+    for (int i = index; i < totalStudents - 1; i++) {
+        rollNo[i] = rollNo[i + 1];
+        name[i] = name[i + 1];
+        meals[i] = meals[i + 1];
+    }
+
+    totalStudents--;
+    cout << "Student record deleted successfully!\n";
+}
+
 int main() {
     int choice;
     
@@ -54,15 +83,18 @@ int main() {
         cout << "1. Add Student\n";
         cout << "2. Display All Students\n";
         cout << "3. Update Student Meals\n";
+        cout << "4. Delete Student Record\n";
         cout << "6. Save & Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
             
-            case 1: addStudent();
+            case 1: addStudent(); break;
             case 2: displayStudents(); break;
-            case 6: saveStudents();
+            case 3: updateMeals(); break;
+            case 4: deleteStudent(); break;
+            case 6: saveStudents(); break;
             
             default: cout << "Invalid choice! Try again.\n";
         }
